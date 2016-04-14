3ab912eeb2c4f1a807bd455de0cef78885f170cc
Aregbede Ayomide <ayo.aregbede@gmail.com>
2016-04-14 07:57:40 +0100

    render contact group

--- src/containers/contact-screen.js
+++ src/containers/contact-screen.js
@@ -47,6 +47,14 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         color: "#1abc9c"
     },
+    headSec:{
+        margin:12,
+    },
+    tag:{
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#1abc9c"
+    },
     rowContainer: {
         borderBottomWidth: 1,
         borderBottomColor: "#e5e5e5",
@@ -68,13 +76,18 @@ const styles = StyleSheet.create({
 
 
 const dummyContacts = [
+    {name:"A", isGroup:true},
     {name: "Aregbede Ayomide"},
     {name: "Andrew Johnson"},
+    {name:"B", isGroup:true},
     {name: "Blake Haskell"},
     {name: "Blaine Hatab"},
+    {name:"D", isGroup:true},
     {name: "Dan Abranamov"},
     {name: "Dunderfelt Samuel"},
+    {name:"S", isGroup:true},
     {name: "Scott J"},
+    {name:"J", isGroup:true},
     {name: "Jim Web"}
 ]
 
@@ -85,7 +98,8 @@ export default class ContactScreen extends Component {
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2});
         this.state = {
             navigationOpen: false,
-            contacts: ds
+            contacts: ds,
+            groups:[]
         }
     }
 
@@ -94,16 +108,26 @@ export default class ContactScreen extends Component {
             contacts: this.state.contacts.cloneWithRows(dummyContacts)
         })
     }
-
-    renderRow = (rowData) => {
-        return (<View style={styles.rowContainer}>
-            { rowData.name.split(" ").map((content,index) => {
+    renderName = (row) => {
+        if(row.isGroup){
+            return <Text style={styles.tag}>{row.name}</Text>
+        } else {
+            return row.name.split(" ").map((content,index) => {
                 return <Text style={index != 0 ? styles.last_name : styles.first_name} key={index}>
                     {content}</Text>
-            })}
-
-        </View>)
+            })
+        }
     }
+    renderRow = (rowData) => {
+        return (
+
+                <View style={rowData.isGroup ? styles.headSec : styles.rowContainer}>
+                    {this.renderName(rowData)}
+                </View>
+        )
+    }
+
+
     toggleNav = () => {
         this.setState({navigationOpen: !this.state.navigationOpen})
     }
