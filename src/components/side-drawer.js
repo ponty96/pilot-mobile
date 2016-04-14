661ad6922c9ea80b363025c91a3fc0f2b2351a31
Aregbede Ayomide <ayo.aregbede@gmail.com>
2016-04-14 06:06:39 +0100

    add dummy images to side menu

--- src/components/side-drawer.js
+++ src/components/side-drawer.js
@@ -12,13 +12,13 @@ import React,{
 } from 'react-native';
 
 const sideBarActionsList = [
-    {image:'', name:"Conversation", count:23},
-    {image:'', name:"Trash", count:23},
-    {image:'', name:"Later", count:23},
-    {image:'', name:"Archive", count:0},
-    {image:'', name:"Contacts", count:23},
-    {image:'', name:"Settings", count:null},
-    {image:'', name:"Nicolas Prieto", count:null}
+    {image:require('./../assets/images/conversation.png'), name:"Conversations", count:23, active:true},
+    {image:require('./../assets/images/trash.png'), name:"Trash", count:14, active:false},
+    {image:require('./../assets/images/later.png'), name:"Later", count:2, active:false},
+    {image:require('./../assets/images/archive.png'), name:"Archive", count:0, active:false},
+    {image:require('./../assets/images/contacts.png'), name:"Contacts", count:23, active:false},
+    {image:require('./../assets/images/settings.png'), name:"Settings", count:null, active:false},
+    {image:require('./../assets/images/jbhatab.png'), name:"Blaine Hatab", count:null, active:false}
 ]
 
 const styles = StyleSheet.create({
@@ -26,15 +26,36 @@ const styles = StyleSheet.create({
         flex:1,
         backgroundColor:"#34495e"
     },
+    rowSpace:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        padding:10
+    },
+    rowSpaceActive:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        padding:10,
+        backgroundColor:"rgba(255,255, 255, 0.15)"
+    },
     row:{
         flexDirection:"row"
+    },
+    image:{
+        height:25,
+        width:25,
+        margin:5
+    },
+    text:{
+        color:"#fff",
+        fontSize:15,
+        margin:10
     }
 });
 
 export default class SideDrawer extends Component {
     constructor(props){
         super(props);
-        const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 != r2})
+        const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 != r2});
         this.state = {
             dataSource:ds
         }
@@ -46,10 +67,15 @@ export default class SideDrawer extends Component {
         })
     }
     renderRow = (rowData) => {
+        const rowStyle = rowData.active ? styles.rowSpaceActive : styles.rowSpace
+        console.log(rowStyle)
         return (
-            <View style={styles.row}>
-                <Text>{rowData.name}</Text>
-                <Text>{rowData.count !== null ? rowData.count : ""}</Text>
+            <View style={rowStyle}>
+                <View style={styles.row}>
+                    <Image source={rowData.image} style={styles.image}/>
+                    <Text style={styles.text}>{rowData.name}</Text>
+                </View>
+                <Text style={styles.text}>{rowData.count !== null ? rowData.count : ""}</Text>
             </View>
         )
     }
